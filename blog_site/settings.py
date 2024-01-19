30d14e4b755d13909113897281e5037c0bb113e8
Thushar <tthushar2004@gmail.com>
2024-01-19 21:27:02 +0530

    changes

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -126,6 +126,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    
+    ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 
